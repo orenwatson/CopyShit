cfcc5ac66bb76cfd85236c920c51690713812d8c
Oren <Oren@OREN-BIG>
2023-03-14 02:53:34 -0400

    Add project files.

---
+++ CopyShit/Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.KbpsBox = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BlockNumBox = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BlockCountBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SinceLastBlockBox = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TimeLeftBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OverCheck = New System.Windows.Forms.CheckBox()
+        Me.ExistCheck = New System.Windows.Forms.CheckBox()
+        Me.ValidCheck = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Title = "FILE TO READ"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(85, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(703, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "READ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 74)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 26)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "WRITE"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(86, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(702, 23)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(292, 129)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(202, 74)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "COPY"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 295)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(776, 27)
+        Me.ProgressBar1.Step = 0
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 328)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(776, 110)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(327, 260)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 14)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Avg Kbps"
+        '
+        'KbpsBox
+        '
+        Me.KbpsBox.Location = New System.Drawing.Point(395, 257)
+        Me.KbpsBox.Name = "KbpsBox"
+        Me.KbpsBox.ReadOnly = True
+        Me.KbpsBox.Size = New System.Drawing.Size(140, 23)
+        Me.KbpsBox.TabIndex = 10
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'BlockNumBox
+        '
+        Me.BlockNumBox.Location = New System.Drawing.Point(289, 227)
+        Me.BlockNumBox.Name = "BlockNumBox"
+        Me.BlockNumBox.ReadOnly = True
+        Me.BlockNumBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.BlockNumBox.Size = New System.Drawing.Size(110, 23)
+        Me.BlockNumBox.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(190, 230)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 14)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Copying Block"
+        '
+        'BlockCountBox
+        '
+        Me.BlockCountBox.Location = New System.Drawing.Point(457, 227)
+        Me.BlockCountBox.Name = "BlockCountBox"
+        Me.BlockCountBox.ReadOnly = True
+        Me.BlockCountBox.Size = New System.Drawing.Size(100, 23)
+        Me.BlockCountBox.TabIndex = 13
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(405, 230)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 14)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Out Of"
+        '
+        'SinceLastBlockBox
+        '
+        Me.SinceLastBlockBox.Location = New System.Drawing.Point(211, 257)
+        Me.SinceLastBlockBox.Name = "SinceLastBlockBox"
+        Me.SinceLastBlockBox.ReadOnly = True
+        Me.SinceLastBlockBox.Size = New System.Drawing.Size(110, 23)
+        Me.SinceLastBlockBox.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(101, 260)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(93, 14)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "ms Since Last"
+        '
+        'SaveFileDialog1
+        '
+        '
+        'TimeLeftBox
+        '
+        Me.TimeLeftBox.Location = New System.Drawing.Point(610, 257)
+        Me.TimeLeftBox.Name = "TimeLeftBox"
+        Me.TimeLeftBox.ReadOnly = True
+        Me.TimeLeftBox.Size = New System.Drawing.Size(128, 23)
+        Me.TimeLeftBox.TabIndex = 17
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(541, 260)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 14)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Time Left"
+        '
+        'OverCheck
+        '
+        Me.OverCheck.AutoSize = True
+        Me.OverCheck.Location = New System.Drawing.Point(27, 164)
+        Me.OverCheck.Name = "OverCheck"
+        Me.OverCheck.Size = New System.Drawing.Size(84, 18)
+        Me.OverCheck.TabIndex = 19
+        Me.OverCheck.Text = "Overwrite"
+        Me.OverCheck.UseVisualStyleBackColor = True
+        '
+        'ExistCheck
+        '
+        Me.ExistCheck.AutoSize = True
+        Me.ExistCheck.Enabled = False
+        Me.ExistCheck.Location = New System.Drawing.Point(27, 142)
+        Me.ExistCheck.Name = "ExistCheck"
+        Me.ExistCheck.Size = New System.Drawing.Size(62, 18)
+        Me.ExistCheck.TabIndex = 20
+        Me.ExistCheck.Text = "Exists"
+        Me.ExistCheck.UseVisualStyleBackColor = True
+        '
+        'ValidCheck
+        '
+        Me.ValidCheck.AutoSize = True
+        Me.ValidCheck.Location = New System.Drawing.Point(27, 185)
+        Me.ValidCheck.Name = "ValidCheck"
+        Me.ValidCheck.Size = New System.Drawing.Size(76, 18)
+        Me.ValidCheck.TabIndex = 21
+        Me.ValidCheck.Text = "Validate"
+        Me.ValidCheck.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ValidCheck)
+        Me.Controls.Add(Me.ExistCheck)
+        Me.Controls.Add(Me.OverCheck)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TimeLeftBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.SinceLastBlockBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.BlockCountBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.BlockNumBox)
+        Me.Controls.Add(Me.KbpsBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "CopyShit.EXE"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents LastblocktimeBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents KbpsBox As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents BlockNumBox As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BlockCountBox As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents SinceLastBlockBox As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TimeLeftBox As TextBox
+    Friend WithEvents OverCheck As CheckBox
+    Friend WithEvents ExistCheck As CheckBox
+    Friend WithEvents ValidCheck As CheckBox
+End Class
---
+++ CopyShit/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Linq.Expressions
+
+Public Class Form1
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        TextBox1.Text = OpenFileDialog1.FileName
+        If TextBox2.Text <> "" Then
+            If File.Exists(TextBox2.Text) Then
+                TextBox2.Text = Path.GetDirectoryName(TextBox2.Text)
+            End If
+            If Directory.Exists(TextBox2.Text) Then
+                TextBox2.Text = Path.Combine(TextBox2.Text, Path.GetFileName(TextBox1.Text))
+            End If
+            TextBox2_TextChanged(sender, e)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+    End Sub
+
+    Private StartTime, LastBlock As DateTime
+    Private Copying As Boolean = False
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Not BackgroundWorker1.IsBusy Then
+            Dim rdfn, wrfn As String
+            rdfn = TextBox1.Text
+            wrfn = TextBox2.Text
+            Button1.Enabled = False
+            Button2.Enabled = False
+            TextBox1.Enabled = False
+            TextBox2.Enabled = False
+            Button3.Enabled = False
+            OverCheck.Enabled = False
+            ValidCheck.Enabled = False
+            Button3.Text = "COPYING"
+            ProgressBar1.Minimum = 0
+            ProgressBar1.Value = 0
+            StartTime = Now
+            Copying = True
+            BackgroundWorker1.RunWorkerAsync((rdfn, wrfn, OverCheck.CheckState, ValidCheck.Checked))
+        End If
+    End Sub
+
+    Private Sub ReenableEverything()
+        Button1.Enabled = True
+        Button2.Enabled = True
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        Button3.Enabled = True
+        ValidCheck.Enabled = True
+        Copying = False
+        Button3.Text = "COPY"
+    End Sub
+
+    Private Sub UpdateShit()
+        If Not Copying Then Return
+        Dim kbps, curtime As Double
+        curtime = (Now - StartTime).TotalMilliseconds
+        If curtime < 1 Then Return
+        kbps = (ProgressBar1.Value / curtime)
+        KbpsBox.Text = kbps.ToString()
+        KbpsBox.Update()
+        SinceLastBlockBox.Text = (Now - LastBlock).TotalMilliseconds
+        SinceLastBlockBox.Update()
+        If kbps < 1 Then Return
+        TimeLeftBox.Text = TimeSpan.FromMilliseconds((ProgressBar1.Maximum - ProgressBar1.Value) / kbps).ToString()
+        TimeLeftBox.Update()
+        ProgressBar1.Update()
+        BlockCountBox.Update()
+        BlockNumBox.Update()
+        TextBox3.Update()
+    End Sub
+
+    Private Sub ReportProg(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Maximum = e.UserState.Item2
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Value = e.UserState.Item1
+        BlockCountBox.Text = Int(e.UserState.Item2 / 2048)
+        BlockNumBox.Text = e.UserState.Item1 / 2048
+        TextBox3.Text = e.UserState.Item3
+        LastBlock = Now
+    End Sub
+
+    Private Sub FinishWork(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        ReenableEverything()
+    End Sub
+
+    Private Function CheckBlock(ByRef rdfl As FileStream, ByRef wrfl As FileStream, pos As Long, siz As Long, ByRef buf() As Byte, ByRef buf2() As Byte) As Boolean
+        rdfl.Seek(pos, SeekOrigin.Begin)
+        rdfl.Read(buf, 0, siz)
+        wrfl.Seek(pos, SeekOrigin.Begin)
+        wrfl.Read(buf2, 0, siz)
+        Return buf.SequenceEqual(buf2)
+    End Function
+
+    Private Sub CopyBlock(ByRef rdfl As FileStream, ByRef wrfl As FileStream, pos As Long, siz As Long, ByRef buf() As Byte)
+        rdfl.Seek(pos, SeekOrigin.Begin)
+        rdfl.Read(buf, 0, siz)
+        wrfl.Seek(pos, SeekOrigin.Begin)
+        wrfl.Write(buf, 0, siz)
+        wrfl.Flush()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim worker As BackgroundWorker = TryCast(sender, BackgroundWorker)
+        Dim rdfl, wrfl As FileStream
+        rdfl = Nothing
+        wrfl = Nothing
+        Dim pos, tot, totwr As Long
+        pos = 0
+        tot = 2
+        totwr
+        Dim ovwr As CheckState = e.Argument.Item3
+        Dim vldt As Boolean = e.Argument.Item4
+        Try
+            Const blksiz As Long = 2048
+            rdfl = File.OpenRead(e.Argument.Item1)
+            wrfl = File.Open(e.Argument.Item2, FileMode.OpenOrCreate)
+            rdfl.Seek(0, SeekOrigin.End)
+            worker.ReportProgress(0, (0, tot, "Beginning Copy"))
+ValidLabel:
+            wrfl.Seek(0, SeekOrigin.End)
+            totwr = wrfl.Position
+            pos = 0
+            Dim buffer(blksiz - 1) As Byte
+            Dim vldbuf(blksiz - 1) As Byte
+            If ovwr <> CheckState.Checked Then
+                worker.ReportProgress(0, (pos, tot, "Checking Existing..."))
+                While pos < totwr - blksiz
+                    If Not CheckBlock(rdfl, wrfl, pos, blksiz, buffer, vldbuf) Then
+                        wrfl.Seek(pos, SeekOrigin.Begin)
+                        wrfl.Write(buffer, 0, blksiz)
+                    End If
+                    pos = pos + blksiz
+                    worker.ReportProgress(0, (pos, tot, "Checking Existing..."))
+                End While
+                If totwr = tot AndAlso (pos = tot OrElse CheckBlock(rdfl, wrfl, pos, tot - pos, buffer, vldbuf)) Then
+                    GoTo CloseLabel
+                End If
+            End If
+WriteLabel:
+            While pos < tot - blksiz
+                CopyBlock(rdfl, wrfl, pos, blksiz, buffer)
+                pos = pos + blksiz
+                worker.ReportProgress(0, (pos, tot, "Copying..."))
+            End While
+            If pos <> tot Then
+                CopyBlock(rdfl, wrfl, pos, tot - pos, buffer)
+            End If
+            worker.ReportProgress(0, (tot, tot, "Flushing..."))
+CloseLabel:
+            If vldt Then
+                vldt = False
+                ovwr = CheckState.Unchecked
+                GoTo ValidLabel
+            End If
+            rdfl.Dispose()
+            wrfl.Dispose()
+            worker.ReportProgress(0, (tot, tot, "Done."))
+        Catch ex As Exception
+            worker.ReportProgress(0, (pos, tot, "Error: " & ex.ToString()))
+            If rdfl IsNot Nothing Then rdfl.Dispose()
+            If wrfl IsNot Nothing Then wrfl.Dispose()
+        End Try
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As CancelEventArgs) Handles SaveFileDialog1.FileOk
+        TextBox2.Text = SaveFileDialog1.FileName
+        TextBox2_TextChanged(sender, e)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        ExistCheck.Checked = File.Exists(TextBox2.Text)
+        If Not ExistCheck.Checked Then
+            OverCheck.CheckState = CheckState.Indeterminate
+            OverCheck.Enabled = False
+        Else
+            OverCheck.Enabled = True
+            OverCheck.Checked = False
+        End If
+        ExistCheck.Update()
+        OverCheck.Update()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        UpdateShit()
+    End Sub
+
+End Class
